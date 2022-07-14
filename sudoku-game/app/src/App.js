007b951efcc0e187932d706ebbebcf81937af2e8
Vignesh Iyer <1792167@tcs.com>
2022-07-14 17:10:52 +0530

    refactoring code

--- sudoku-game/app/src/App.js
+++ sudoku-game/app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <br></br>
-        <p>Vignesh --- </p>
+        <p>CHOKIDAR</p>
         <a
           className="App-link"
           href="https://reactjs.org"
